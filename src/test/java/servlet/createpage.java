b83e98880026006c2dd1e499e048b88b7afbf4a7
hariharansethu <38229092+hariharansethu@users.noreply.github.com>
2020-04-02 13:13:15 -0500

    Update createpage.java
    Updated IP address with aws

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.141.142.198:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.216.142.137:5432/postgres";
 			String username="postgres";
 			String password="";
 		
